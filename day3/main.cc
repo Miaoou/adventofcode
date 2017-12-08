4e39682e2ec7d4af8fe900071e6e733f65beac60
Miaou <miaoou@gmail.com>
2017-12-08 01:14:46 +0100

    test backup

--- day3/main.cc
+++ day3/main.cc
@@ -8,28 +8,49 @@
 #include <iterator>
 #include <set>
 #include <array>
+#include <cmath>
 
 using namespace std;
 
-template< typename T, T... Ns >
-auto constexpr
-make_data( std::integer_sequence< T, Ns... > ) noexcept
-    -> array< T, sizeof...( Ns ) > {
-    return {{ Ns... }};
-}
-
-template< typename T, size_t N >
-auto constexpr
-make_data() noexcept {
-    return make_data< T >( std::make_integer_sequence< T, N >{} );
-}
-
 uint32_t
 part1() {
-    auto constexpr data = make_data< uint32_t, 12 >();
-    uint32_t dist = data.size();
+    auto constexpr n = 12;
+
+    if (n == 1)
+        return 0;
     
-    return dist;
+    uint32_t nb_full_lines = 1;
+
+    for (auto i = 1; i <= 12; ++i) {
+        if (i == pow(nb_full_lines, 2)) {
+            nb_full_lines += 2;
+        }
+    }
+
+    uint32_t last_corner = pow(nb_full_lines, 2); // 9
+
+    auto top_right = last_corner + nb_full_lines + 1; // 13
+    auto top_left = top_right + nb_full_lines + 1; // 17
+    auto bot_left = top_left + nb_full_lines + 1; // 21
+    auto bot_right = bot_left + nb_full_lines + 1; // 25
+
+    auto curr_case = 0;
+    for (auto i = last_corner; i <= bot_right && i != n; ++i) {
+        if (i == top_right || i == top_left || i == bot_left || i == bot_right)
+            curr_case = 0;
+        else
+            ++curr_case;
+    }
+    ++curr_case;
+
+    if (curr_case == 0)
+        return 2 * (nb_full_lines - 1);
+    else
+        return nb_full_lines - 1 + ((curr_case - 1) / 2);
+    
+
+
+    return 0;
 }
 
 int
