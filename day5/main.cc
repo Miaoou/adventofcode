537416ac23609d2cea3cdff4a5134fab00318a6f
Miaou <miaoou@gmail.com>
2017-12-18 19:02:15 +0100

    day5 part 1

---
+++ day5/main.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sstream>
+#include <cstdint>
+#include <streambuf>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <iterator>
+#include <set>
+#include <array>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <optional>
+#include <memory>
+#include <fstream>
+#include <numeric>
+
+using namespace std;
+
+uint32_t
+part1() {
+    uint32_t steps = 0;
+    ifstream ifs( "input.txt" );
+    vector< int32_t > data( istream_iterator< int32_t >( ifs ), {} );
+
+    for( auto p = begin( data ); p != end( data ); ++steps )
+        p += ( *p )++;
+
+    return steps;
+}
+
+int
+main() {
+    cout << part1();
+
+    return 0;
+}
