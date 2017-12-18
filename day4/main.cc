3918c584992d521d5d72f8843c9b2363c6d8152b
Brice Gagnage <bgagnage@tp.michelin.com>
2017-12-18 14:20:50 +0100

    more elegant code

--- day4/main.cc
+++ day4/main.cc
@@ -50,14 +50,11 @@ part2() {
         set< string > words;
         istringstream sline( line );
 
-        bool valid = true;
-        for( string word; sline >> word; ) {
+        nb_valid += all_of( istream_iterator< string >( sline ), {}, [&words] ( auto word ) {
             sort( begin( word ), end( word ) );
-            if( !words.insert( word ).second )
-                valid = false;
-        }
+            return words.insert( word ).second;
+        } );
 
-        nb_valid += valid ? 1 : 0;
     }
 
     return nb_valid;
